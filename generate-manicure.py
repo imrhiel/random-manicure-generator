83e15552c1c04059f2a280f621ed0164b11ef873
Elias <enki@faloran.fr>
2023-05-24 13:04:03 +0200

    Adding a function for the random roll on a list

--- generate-manicure.py
+++ generate-manicure.py
@@ -7,16 +7,21 @@ import random
 base_type = ['creme', 'linear', 'multichrome']
 taco_type = ['glossy', 'matte']
 
+# Randomly selecting on a list
+def roll_polish(polish_type) :
+    return polish_type[random.randint(0,len(polish_type)-1)]
+    
+
 # Select a random nail polish from the list
 def select_random_polish(polish_type) :
     polish_list =  open('files/%s.txt' % polish_type).read().strip('\n').split('\n')
-    polish = polish_list[random.randint(0,len(polish_list)-1)]
+    polish = roll_polish(polish_list)
     return(polish)
 
 print('Welcome to the Random Manicure Generator!\n')
 
 # Randomly select base polish
-base = base_type[random.randint(0,len(base_type)-1)]
+base = roll_polish(base_type)
 print('Your base type is : %s' % base)
 polish = select_random_polish(base)
 print('Your random polish is : %s\n' % polish)
@@ -30,7 +35,7 @@ if input_topper.lower() == 'y' or  input_topper == '':
 # Select taco if needed
 input_taco = input("Do you wish to roll for a taco (top coat)? (y/n):")
 if input_taco.lower() == 'y':
-    taco = taco_type[random.randint(0,len(taco_type)-1)]
+    taco = roll_polish(taco_type)
     print('Your taco is: %s' % taco)
 else:
     print('Please still use a taco!')
